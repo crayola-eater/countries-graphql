9c3c24993cfa9c8434ff0392392fe1e65f90c97e
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2022-11-18 10:22:32 +0000

    Adding context provider with Apollo client

---
+++ src/contexts/ApolloProvider.tsx
@@ -0,0 +1,15 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import type { ReactNode } from "react";
+
+const client = new ApolloClient({
+  uri: "https://countries.trevorblades.com/graphql",
+  cache: new InMemoryCache(),
+});
+
+type ApolloProviderProps = {
+  children: ReactNode | ReactNode[] | null;
+};
+
+export default function ApolloProviderWithClient(props: ApolloProviderProps) {
+  return <ApolloProvider client={client}>{props.children}</ApolloProvider>;
+}
